d485618a361210c4f5e555f7f8764a59cd7dd1c8
ayeshaaamir <ayeshaaamir38@gmail.com>
2024-11-08 23:29:38 +0000

    helpdesk intial setup..

---
+++ helpdesk-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600">
+          Test IBS
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
