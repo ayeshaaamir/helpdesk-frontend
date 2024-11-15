356352dd52011e07920c36262cf3c59f1d94ba36
ayeshaaamir <ayeshaaamir38@gmail.com>
2024-11-15 15:21:17 +0000

    MUI added..

--- helpdesk-frontend/src/pages/Home.tsx
+++ helpdesk-frontend/src/pages/Home.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 const Home: React.FC = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <div className="p-4 bg-gray-100">
+        <h1 className="text-2xl font-bold mb-4">
+          Hello, Tailwind and Material UI!
+        </h1>
+        <Button
+          variant="contained"
+          className="bg-blue-500 hover:bg-blue-700 text-white"
+        >
+          Material UI Button
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- helpdesk-frontend/src/pages/NotFound.tsx
+++ helpdesk-frontend/src/pages/NotFound.tsx
@@ -1,33 +1,36 @@
 import { useNavigate } from "react-router-dom";
-import styles from "./NotFound.module.css";
 
 const NotFound = () => {
   const navigate = useNavigate();
 
   const handleGoBack = () => {
     navigate(-1);
-  }
+  };
 
   const handleGoHome = () => {
-    navigate('/');
-  }
+    navigate("/");
+  };
   return (
     <>
-      <div className={styles.noise}></div>
-      <div className={styles.overlay}></div>
-      <div className={styles.terminal}>
-        <h1>
-          Error <span className={styles.errorcode}>404</span>
-        </h1>
-        <p className={styles.output}>
-          The page you are looking for might have been removed, had its name
-          changed or is temporarily unavailable.
-        </p>
-        <p className={styles.output}>
-          Please try to <a className="cursor-pointer" onClick={handleGoBack}>go back</a> or{" "}
-          <a className="cursor-pointer" onClick={handleGoHome}>return to the homepage</a>.
-        </p>
-        <p className={styles.output}>Good luck.</p>
+      <div className="flex items-center justify-center h-screen bg-black">
+        <div className="text-center">
+          <h1 className="text-9xl font-bold text-white">404</h1>
+          <p className="mt-4 text-xl text-gray-400">
+            Oops! The page you are looking for doesn't exist.
+          </p>
+          <a
+            onClick={handleGoHome}
+            className="cursor-pointer mt-6 inline-block px-6 py-2 text-lg font-medium text-black bg-white rounded hover:bg-gray-200 ml-4"
+          >
+            Home
+          </a>
+          <a
+            onClick={handleGoBack}
+            className="cursor-pointer mt-6 inline-block px-6 py-2 text-lg font-medium text-black bg-white rounded hover:bg-gray-200 ml-4"
+          >
+            Go Back
+          </a>
+        </div>
       </div>
     </>
   );
